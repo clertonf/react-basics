b89b1db6514f0dfffde4b07cbb5e1a8d321b042d
clertonf <clerton.filho7@gmail.com>
2021-03-06 12:49:57 -0300

    subindo back e front

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,7 +1,11 @@
 module.exports = {
     presets: [
       '@babel/preset-env', 
-      '@babel/preset-react'  
+      '@babel/preset-react',
+
       //Conjunto de configurações criada por terceiros
     ],
+    plugins: [
+      '@babel/plugin-transform-runtime'
+    ]
 };
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import Header from './components/Header';
+
+import './App.css';
+// import backgroundImage from './assets/background.jpeg';
 
 /**
  * Componente
@@ -12,27 +16,42 @@ import Header from './components/Header';
  */
 
 function App(){
-    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get('projects').then(response => {
+           setProjects(response.data);
+        }); //buscar GET
+    }, [] ); //Array de dependencias
 
     // useState retorna um array com 2 posições
     //
     // 1.Variável com o seu valor inicial
     // 2. Função para atualizarmos esse valor 
     
-    function handleAddProject(){
+   async function handleAddProject(){
        // projects.push(`Novo projeto ${Date.now()}`);
 
-       setProjects([...projects, `Novo projeto ${Date.now()}`]);
+    //    setProjects([...projects, `Novo projeto ${Date.now()}`]);
 
-        console.log(projects);
+        const response = await api.post('projects', {
+            title: `Novo projeto ${Date.now()}`,
+            owner: "Clerton Filho"
+        });
+
+        const project = response.data;
+
+        setProjects([...projects, project]);
     }
     
     return (
         <>
             <Header title="Projects"/>
+
+            {/* <img width={300} src={backgroundImage}/> */}
            
             <ul>
-                {projects.map(project => <li key={project}>{project}</li>)} 
+                {projects.map(project => <li key={project.id}>{project.title}</li>)} 
             </ul>
 
             <button type='button' onClick={handleAddProject}>Adicionar projeto</button>
